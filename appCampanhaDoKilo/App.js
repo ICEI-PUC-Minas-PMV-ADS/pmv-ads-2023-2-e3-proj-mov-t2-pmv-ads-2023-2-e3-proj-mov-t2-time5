0ceb7c5a131b570fd1325913762a78a8881440ac
Astor Almeida <astormg@gmail.com>
2023-09-21 11:40:19 -0300

    feat: Tela de Login

--- appCampanhaDoKilo/App.js
+++ appCampanhaDoKilo/App.js
@@ -1,10 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.container.title}>Campanha do Kilo</Text>
+      <Text>Login</Text>
+      <TextInput style={styles.container.border} />
+      <Text>Senha</Text>
+      <TextInput style={styles.container.border} />
+      <Text style={styles.container.cadastro}>Não possui login? Clique Aqui</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     title:{fontSize:28},
+    border: {borderWidth: 1, borderColor: '#d5d5d5', width: 100,},
+    cadastro: {fontSize:8}
   },
 });
